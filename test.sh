f764fe9421991c503b739cb2770d02ef6cc6184b
Carlos Chinchilla <calufa@gmail.com>
2021-09-04 20:36:01 -0600

    add docker-compose

---
+++ test.sh
@@ -0,0 +1,13 @@
+APP_NAME=${1}
+if [ "${APP_NAME}" = "" ]
+	then
+		echo "ERROR: enter an application name"
+		exit 1
+	else
+		FILE_PATH="${APP_NAME}/test.yaml"
+fi
+docker-compose -f ${FILE_PATH} down --remove-orphans
+docker-compose -f ${FILE_PATH} build main
+docker-compose -f ${FILE_PATH} run \
+	--name ${APP_NAME} \
+	main
