ae94e2e788f40e3fe1042e39b05a2df011fd390f
Carlos Chinchilla <calufa@gmail.com>
2021-09-11 13:03:05 -0600

    use dev image

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-APP_NAME=${1}
+export APP_NAME=${1}
 if [ "${APP_NAME}" = "" ]
 	then
 		echo "ERROR: enter an application name"
@@ -7,6 +7,10 @@ if [ "${APP_NAME}" = "" ]
 		FILE_PATH="${APP_NAME}/test.yaml"
 fi
 docker-compose -f ${FILE_PATH} down --remove-orphans
+
+docker-compose -f ${FILE_PATH} build build_base_image
+docker-compose -f ${FILE_PATH} run build_base_image
+
 docker-compose -f ${FILE_PATH} build main
 docker-compose -f ${FILE_PATH} run \
 	--name ${APP_NAME} \
