f4d441936440abf43e0b1ba95682643bf5de6493
Carlos Chinchilla <calufa@gmail.com>
2021-09-16 12:48:10 -0600

    remove Dockerfile.base

--- test.sh
+++ test.sh
@@ -7,10 +7,6 @@ if [ "${APP_NAME}" = "" ]
 		FILE_PATH="${APP_NAME}/test.yaml"
 fi
 docker-compose -f ${FILE_PATH} down --remove-orphans
-
-docker-compose -f ${FILE_PATH} build build_base_image
-docker-compose -f ${FILE_PATH} run build_base_image
-
 docker-compose -f ${FILE_PATH} build main
 docker-compose -f ${FILE_PATH} run \
 	--name ${APP_NAME} \
