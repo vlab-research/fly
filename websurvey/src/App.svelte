e9057fde4ca5e23fa158fbd97649836e68297a89
Sophie <sophie@sssoja.com>
2021-11-11 12:48:49 +0100

    dynamically render basic fields data

--- websurvey/src/App.svelte
+++ websurvey/src/App.svelte
@@ -1,7 +1,22 @@
 <script>
+	import typeformData from "./typeformData.js";
 
+	const { fields } = typeformData;
+	const { length } = fields;
 </script>
 
 <main>
-
+	<form>
+		<div>
+			<!-- Fields -->
+			{#each fields as field, index (field.id)}
+				<label for="question-0">Question
+					{index + 1}
+					out of
+					{length}</label>
+				<li>{field.title}</li>
+				<input />
+			{/each}
+		</div>
+	</form>
 </main>
