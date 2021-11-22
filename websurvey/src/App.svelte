ae78698717fbfdbd68dab6090def3e17d263cea8
Sophie <sophie@sssoja.com>
2021-11-22 12:30:52 +0100

    reimplement linear navigate functionality on form submit

--- websurvey/src/App.svelte
+++ websurvey/src/App.svelte
@@ -2,21 +2,31 @@
 	import { Router, Route, Link } from "svelte-routing";
 	import Home from "./routes/Home.svelte";
 	import Form from "./routes/Form.svelte";
+	import typeformData from "./typeformData.js";
 
 	export let url = "";
+
+	const { fields } = typeformData;
+
+	let ref = fields[0].ref;
+
+	const updateRef = (e) => {
+		const updatedRef = e.detail;
+		ref = updatedRef;
+	};
 </script>
 
 <main>
 	<Router {url}>
 		<nav>
 			<Link to="/">Home</Link>
-			<Link to="/:ref">Question</Link>
+			<Link to="/{ref}">Question</Link>
 		</nav>
 		<Route path="/">
 			<Home />
 		</Route>
 		<Route path="/:ref" let:params>
-			<Form ref={params.ref} />
+			<Form ref={params.ref} on:updateRef={updateRef} />
 		</Route>
 	</Router>
 </main>
--- websurvey/src/routes/Form.svelte
+++ websurvey/src/routes/Form.svelte
@@ -1,8 +1,11 @@
 <script>
+    import { navigate } from "svelte-routing";
     import { createEventDispatcher } from "svelte";
     import MultipleChoice from "../components/MultipleChoice.svelte";
     import ShortText from "../components/ShortText.svelte";
     import typeformData from "../typeformData.js";
+
+    let dispatch = createEventDispatcher();
 
     export let ref;
 
@@ -11,13 +14,14 @@
 
     let index = 0;
 
-    let field = fields[index];
+    let currentField = fields[index];
 
     const handleSubmit = () => {
         if (index < fields.length - 1) index++;
-        field = fields[index];
-        ref = field.ref;
-        console.log(field, ref);
+        currentField = fields[index];
+        ref = currentField.ref;
+        navigate(`/${ref}`, { replace: true });
+        dispatch("updateRef", ref);
     };
 </script>
 
@@ -25,21 +29,23 @@
     <form on:submit|preventDefault={handleSubmit}>
         <div class="stack-small">
             <!-- Question -->
-            {#if field}
-                <h2 class="label-wrapper">
-                    <label for="question-{index + 1}">Question
-                        {index + 1}
-                        out of
-                        {length}</label>
-                </h2>
-                {#if field.type === 'short_text'}
-                    <ShortText {field} />
-                {:else if field.type === 'multiple_choice'}
-                    <MultipleChoice {field} />
-                {:else}
-                    <p>You've reached the end of the survey!</p>
+            {#each fields as field (field.id)}
+                {#if currentField === field}
+                    <h2 class="label-wrapper">
+                        <label for="question-{index + 1}">Question
+                            {index + 1}
+                            out of
+                            {length}</label>
+                    </h2>
+                    {#if field.type === 'short_text'}
+                        <ShortText {field} />
+                    {:else if field.type === 'multiple_choice'}
+                        <MultipleChoice {field} />
+                    {:else}
+                        <p>You've reached the end of the survey!</p>
+                    {/if}
                 {/if}
-            {/if}
+            {/each}
             <button class="btn">OK</button>
         </div>
     </form>
