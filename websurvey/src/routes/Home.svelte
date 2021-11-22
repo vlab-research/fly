359f8b83232e942253b98f23b3ca5008ec2e86f3
Sophie <sophie@sssoja.com>
2021-11-22 13:58:22 +0100

    from the first page navigate to the first question

--- websurvey/src/routes/Home.svelte
+++ websurvey/src/routes/Home.svelte
@@ -1,9 +1,28 @@
 <script>
     import { navigate } from "svelte-routing";
+    import { createEventDispatcher } from "svelte";
+
+    export let fields;
+
+    export let ref;
+
+    let dispatch = createEventDispatcher();
+
+    let index = fields.findIndex((field) => field.ref === ref);
+
+    let field = fields[index];
+
+    const handleSubmit = () => {
+        index += 1;
+        field = fields[index];
+        ref = field.ref;
+        navigate(`/${ref}`, { replace: true });
+        dispatch("updateRef", ref);
+    };
 </script>
 
 <div class="surveyapp stack-large">
-    <form>
+    <form on:submit|preventDefault={handleSubmit}>
         <div class="stack-small">
             <h1 class="h1">Hey! Welcome to our fun survey</h1>
             <button type="submit" class="btn">Let's go</button>
