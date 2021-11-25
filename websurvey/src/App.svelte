d0bc50d5f040140d998d7d5d91ba856c3cbc5425
Sophie <sophie@sssoja.com>
2021-11-25 18:49:19 +0100

    implement routing logic using by binding field to ref

--- websurvey/src/App.svelte
+++ websurvey/src/App.svelte
@@ -3,20 +3,17 @@
 	import Home from "./routes/Home.svelte";
 	import Form from "./routes/Form.svelte";
 	import Thankyou from "./routes/Thankyou.svelte";
-
 	import typeformData from "./typeformData.js";
 
 	export let url = "";
 
 	const { fields, thankyou_screens } = typeformData;
-
-	let ref;
 </script>
 
 <main>
 	<Router {url}>
 		<Route path="/">
-			<Home {ref} {fields} />
+			<Home {fields} />
 		</Route>
 		<Route path="/:ref" let:params>
 			<Form ref={params.ref} {fields} />
--- websurvey/src/routes/Form.svelte
+++ websurvey/src/routes/Form.svelte
@@ -1,30 +1,39 @@
 <script>
     import MultipleChoice from "../components/MultipleChoice.svelte";
     import ShortText from "../components/ShortText.svelte";
+    import { navigate } from "svelte-routing";
 
     export let ref, fields;
 
-    let index = fields.findIndex((field) => field.ref === ref);
+    let index, field;
 
-    $: field = fields[index];
+    $: {
+        index = fields.findIndex((field) => field.ref === ref);
+        field = fields[index];
+    }
+
+    const handleSubmit = () => {
+        if (index < fields.length - 1) {
+            const newRef = fields[index + 1].ref;
+            navigate(`/${newRef}`, { replace: true });
+        }
+    };
 </script>
 
 <div class="surveyapp stack-large">
-    <form>
+    <form on:submit|preventDefault={handleSubmit}>
         <div class="stack-small">
             <!-- Question -->
-            {#if field}
-                <h2 class="label-wrapper">
-                    <label for="question-{index + 1}">Question
-                        {index + 1}
-                        out of
-                        {fields.length}</label>
-                </h2>
-                {#if field.type === 'short_text'}
-                    <ShortText {field} />
-                {:else if field.type === 'multiple_choice'}
-                    <MultipleChoice {field} />
-                {/if}
+            <h2 class="label-wrapper">
+                <label for="question-{index + 1}">Question
+                    {index + 1}
+                    out of
+                    {fields.length}</label>
+            </h2>
+            {#if field.type === 'short_text'}
+                <ShortText {field} />
+            {:else if field.type === 'multiple_choice'}
+                <MultipleChoice {field} />
             {/if}
             <button class="btn">OK</button>
         </div>
