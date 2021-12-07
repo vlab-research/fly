7ed2ca878a76e43f369816d2c842884224c04aca
Sophie <sophie@sssoja.com>
2021-12-07 20:21:53 +0100

    create a helper directory in form.js

--- websurvey/lib/typewheels/form.js
+++ websurvey/lib/typewheels/form.js
@@ -1,6 +1,6 @@
 class FieldError extends Error {}
 
-function getField({ form }, ref) {
+function getField(form, ref) {
   if (!form.fields.length) {
     throw new FieldError(`This form has no fields: ${form.id}`);
   }
@@ -12,85 +12,43 @@ function getField({ form }, ref) {
     throw new FieldError(`Could not find the requested field, ${ref},
                           in our form: ${form.id}!`);
   }
-
   return field;
 }
 
-function getFieldResponse(qa, ref) {
-  // return last valid answer
-  const match = qa.filter(([q, __]) => q === ref).pop();
-  // return null if there are no matches,
-  // or if there are no answers,
-  const answer = match && match[1];
-  return answer ? answer : null;
-}
-
-function getIndex({ form }, ref) {
-  if (!form.fields.length) {
-    throw new FieldError(`This form has no fields: ${form.id}`);
+// should the second arg be ref instead of idx?
+function getThankyouScreen(form, idx) {
+  if (!form.thankyou_screens.length) {
+    throw new FieldError(`This form has no thankyou screens: ${form.id}`);
   }
 
+  const thankyouScreen = form.thankyou_screens[idx];
+
+  if (idx > form.thankyou_screens.length) {
+    throw new FieldError(`Could not find the requested thankyouscreen, at index ${idx},
+                          in our form: ${form.id}!`);
+  }
+
+  return thankyouScreen;
+}
+
+function isLast(form, ref) {
   const idx = form.fields.map(({ ref }) => ref).indexOf(ref);
-  const field = form.fields[idx];
-
-  if (!field) {
-    throw new FieldError(`Could not find the requested field, ${ref},
-                          in our form: ${form.id}`);
-  }
-  return idx;
+  return idx === form.fields.length - 1;
 }
 
-function isLast(fields, ref) {
-  const index = fields.findIndex(field => field.ref === ref);
-  return index === fields.length - 1;
+function setFirstRef(form, idx) {
+  return form.fields[idx].ref;
 }
 
-function getNextRef(fields, ref) {
-  const index = fields.findIndex(field => field.ref === ref);
-  return fields[index + 1].ref;
-}
-
-function getNextField(fields, ref) {
-  if (isLast(fields, ref)) {
-    return null;
-  }
-
-  const index = fields.findIndex(field => field.ref === ref);
-  return fields[index + 1];
-}
-
-function getNextFieldWithLogic(data, field) {
-  const logic =
-    data.logic && data.logic.find(logicItem => logicItem.ref === field.ref);
-
-  if (logic) {
-    const next = jump(data, logic);
-    const field = getField(fields, next);
-    return field;
-  }
-
-  return getNextField(data, field);
-}
-
-function jump(data, logic) {
-  const { ref, actions } = logic;
-
-  for (let { condition, details } of actions) {
-    if (getCondition(data, ref, condition)) {
-      return details.to.value;
-    }
-  }
-
-  // Default to next field if none found
-  return getNextRef(fields, ref);
+function getNextRef(form, ref) {
+  const idx = form.fields.map(({ ref }) => ref).indexOf(ref);
+  return form.fields[idx + 1].ref;
 }
 
 module.exports = {
-  getFieldResponse,
   isLast,
-  getIndex,
-  getNextRef,
   getField,
-  getNextField,
-  getNextFieldWithLogic,
+  setFirstRef,
+  getNextRef,
+  getThankyouScreen,
 };
