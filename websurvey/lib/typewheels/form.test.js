bfa8737403a708b0a07c9dff76940e2c11fbd939
Sophie <sophie@sssoja.com>
2021-12-07 20:21:21 +0100

    create tests for helper functions in form.js

--- websurvey/lib/typewheels/form.test.js
+++ websurvey/lib/typewheels/form.test.js
@@ -1,35 +1,60 @@
 const mocha = require("mocha");
 const chai = require("chai");
 const should = chai.should();
-const f = require("./form");
-const form = require("../../mocks/typeformData.json");
+const f = require("./form.js");
+const fs = require("fs");
+const form = JSON.parse(fs.readFileSync("mocks/sample.json"));
 
 describe("getField", () => {
   it("gets a field", () => {
-    const ctx = { form };
+    const ctx = form;
     const value = f.getField(ctx, "whats_your_name");
     value.should.equal(form.fields[0]);
   });
 
   it("throws with a useful message when field not found in form", () => {
-    const ctx = { form };
+    const ctx = form;
     const fn = f.getField(ctx, "baz");
     fn.should.throw(/baz/); // field
     fn.should.throw(/DjlXLX2s/); // form
   });
 });
 
-describe("getFieldResponse", () => {
-  it("gets the response of a text field", () => {
-    const qa = [["foo", "foo"]];
-    const value = f.getFieldResponse(qa, "foo");
-    value.should.equal("foo");
+describe("getThankyouScreen", () => {
+  it("gets thankyou screen", () => {
+    const ctx = form;
+    const value = f.getThankyouScreen(ctx, 0);
+    value.should.equal(form.thankyou_screens[0]);
   });
 
-  // TODO: should this throw?
-  it("returns null if the field doesnt exist", () => {
-    const qa = [];
-    const value = f.getFieldResponse(qa, "foo");
-    should.not.exist(value);
+  it("throws with a useful message when thankyou screen not found in form", () => {
+    const ctx = form;
+    const fn = f.getThankyouScreen(ctx, 3);
+    fn.should.throw(/3/); // thankyou screen
+    fn.should.throw(/DjlXLX2s/); // form
+  });
+});
+
+describe("isLast", () => {
+  it("checks if a field is last", () => {
+    const ctx = form;
+    const value = f.isLast(ctx, "how_is_your_day");
+    value.should.equal(true);
+  });
+});
+
+describe("setFirstRef", () => {
+  it("sets the first ref", () => {
+    const ctx = form;
+    const value = f.setFirstRef(ctx, 0);
+    value.should.equal("whats_your_name");
+  });
+});
+
+describe("getNextRef", () => {
+  it("gets the next ref", () => {
+    const ctx = form;
+    const value = f.getNextRef(ctx, "whats_your_name");
+    value.should.equal("whats_your_age");
   });
 });
