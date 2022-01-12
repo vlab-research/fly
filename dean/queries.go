49f6d8766f66320a272c35a144db9ee3f3fa0c6e
Carlos Chinchilla <calufa@gmail.com>
2022-01-12 11:10:06 -0600

    format sql

--- dean/queries.go
+++ dean/queries.go
@@ -74,83 +74,88 @@ func getFollowUp(rows pgx.Rows) *ExternalEvent {
 }
 
 func Respondings(cfg *Config, conn *pgxpool.Pool) <-chan *ExternalEvent {
-	query := `SELECT userid, pageid
-              FROM states
-              WHERE
-                current_state = 'RESPONDING' AND
-                updated + ($1)::INTERVAL > $3 AND
-                ($3 - updated) > ($2)::INTERVAL`
-
+	query := `
+		SELECT userid, pageid
+		FROM states
+		WHERE
+			current_state = 'RESPONDING' AND
+			updated + ($1)::INTERVAL > $3 AND
+			($3 - updated) > ($2)::INTERVAL
+	`
 	d := time.Now().UTC()
 	return get(conn, getRedo, query, cfg.RespondingInterval, cfg.RespondingGrace, d)
 }
 
 func Errored(cfg *Config, conn *pgxpool.Pool) <-chan *ExternalEvent {
-
-	query := `SELECT userid, pageid
-              FROM states
-              WHERE
-                current_state = 'ERROR' AND
-                error_tag = ANY($1) AND
-                updated + ($2)::INTERVAL > $3 AND
-                ($3 > next_retry OR next_retry IS NULL)`
-
+	query := `
+		SELECT userid, pageid
+		FROM states
+		WHERE
+			current_state = 'ERROR' AND
+			error_tag = ANY($1) AND
+			updated + ($2)::INTERVAL > $3 AND
+			($3 > next_retry OR next_retry IS NULL)
+	`
 	d := time.Now().UTC()
 	return get(conn, getRedo, query, cfg.ErrorTags, cfg.ErrorInterval, d)
 }
 
 func Blocked(cfg *Config, conn *pgxpool.Pool) <-chan *ExternalEvent {
-
-	query := `SELECT userid, pageid
-              FROM states
-              WHERE
-                current_state = 'BLOCKED' AND
-                fb_error_code = ANY($1) AND
-                updated + ($2)::INTERVAL > $3 AND
-                ($3 > next_retry OR next_retry IS NULL)`
-
+	query := `
+		SELECT userid, pageid
+		FROM states
+		WHERE
+			current_state = 'BLOCKED' AND
+			fb_error_code = ANY($1) AND
+			updated + ($2)::INTERVAL > $3 AND
+			($3 > next_retry OR next_retry IS NULL)
+	`
 	d := time.Now().UTC()
 	return get(conn, getRedo, query, cfg.Codes, cfg.BlockedInterval, d)
 }
 
 func Timeouts(cfg *Config, conn *pgxpool.Pool) <-chan *ExternalEvent {
-	query := `SELECT (state_json->>'waitStart')::int, userid, pageid
-              FROM states
-              WHERE
-                current_state = 'WAIT_EXTERNAL_EVENT' AND
-                timeout_date < $1`
-
+	query := `
+		SELECT (state_json->>'waitStart')::int, userid, pageid
+		FROM states
+		WHERE
+			current_state = 'WAIT_EXTERNAL_EVENT' AND
+			timeout_date < $1
+	`
 	d := time.Now().UTC()
 	return get(conn, getTimeout, query, d)
 }
 
 // TODO: test cockroach perf and index
 func FollowUps(cfg *Config, conn *pgxpool.Pool) <-chan *ExternalEvent {
-	query := `WITH x AS
-                (WITH t AS
-                  (SELECT state_json->>'question' as question, states.userid, states.pageid, surveys.shortcode, has_followup, surveys.created
-				  FROM states
-                                  INNER JOIN credentials c
-                                    ON pageid = facebook_page_id
-				  INNER JOIN surveys
-                                    ON states.current_form = surveys.shortcode
-                                    AND c.userid = surveys.userid
-				  WHERE
-					surveys.created <= form_start_time AND
-					current_state = 'QOUT'  AND
-					previous_is_followup = FALSE AND
-					previous_with_token = FALSE AND
-					(NOW() - updated) > ($1)::INTERVAL AND
-					(NOW() - updated) < ($2)::INTERVAL
-                  )
-                SELECT *, ROW_NUMBER() OVER (PARTITION BY userid, pageid, shortcode ORDER BY created DESC)
-                FROM t
-              )
-              SELECT question, userid, pageid
-              FROM x
-              WHERE
-                row_number = 1 AND
-                has_followup = TRUE`
+	query := `
+		WITH x AS (
+		  WITH t AS (
+		    SELECT state_json->>'question' as question, states.userid, states.pageid, surveys.shortcode, has_followup, surveys.created
+		    FROM states
+		    INNER JOIN credentials c ON pageid = facebook_page_id
+		    INNER JOIN surveys ON states.current_form = surveys.shortcode
+		    	AND c.userid = surveys.userid
+		    WHERE
+		      surveys.created <= form_start_time AND
+		      current_state = 'QOUT'  AND
+		      previous_is_followup = FALSE AND
+		      previous_with_token = FALSE AND
+		      (NOW() - updated) > ($1)::INTERVAL AND
+		      (NOW() - updated) < ($2)::INTERVAL
+		  )
+		  SELECT *, ROW_NUMBER() OVER (
+		    PARTITION BY userid, pageid, shortcode ORDER BY created DESC
+		  ) FROM t
+		)
+		SELECT question, userid, pageid
+		FROM x
+		WHERE
+		  row_number = 1 AND
+		  has_followup = TRUE
+	`
+	return get(conn, getFollowUp, query, cfg.FollowUpMin, cfg.FollowUpMax)
+}
 
 	return get(conn, getFollowUp, query, cfg.FollowUpMin, cfg.FollowUpMax)
 }
