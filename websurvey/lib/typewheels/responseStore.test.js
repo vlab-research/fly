1618171f2a2691e5fc5fab55433374cf09531207
Sophie <sophie@sssoja.com>
2022-01-19 17:48:56 +0100

    add unit tests for the next action method

--- websurvey/lib/typewheels/responseStore.test.js
+++ websurvey/lib/typewheels/responseStore.test.js
@@ -2,8 +2,10 @@ const mocha = require("mocha");
 const chai = require("chai");
 const should = chai.should();
 const r = require("./responseStore");
+const fs = require("fs");
+const form = JSON.parse(fs.readFileSync("mocks/sample.json"));
 
-describe("Test Fly Survey Integration Testing", () => {
+describe("Unit Tests For Response Store", () => {
   it("returns a snapshot of the current field and field value", () => {
     const responseStore = new r.ResponseStore();
     const ref = "what's_your_name";
@@ -31,5 +33,98 @@ describe("Test Fly Survey Integration Testing", () => {
       ["what's_your_name", "foo"],
       ["what's_your_age", 10],
     ]);
+  });
+
+  it("returns an error if an empty answer is submitted to a required question", () => {
+    const responseStore = new r.ResponseStore();
+
+    const field = {
+      type: "short_text",
+      title: "whats_your_name",
+      ref: "whats_your_name",
+    };
+
+    let fieldValue = " ";
+
+    let qa = [["whats_your_name", " "]];
+
+    const ref = "whats_your_name";
+
+    let required = true;
+
+    const value = responseStore.nextAction(
+      form,
+      field,
+      fieldValue,
+      qa,
+      ref,
+      required
+    );
+
+    value.action.should.equal("error");
+  });
+
+  it("instructs the form to throw an error if an answer evaluates to invalid", () => {
+    const responseStore = new r.ResponseStore();
+
+    const field = {
+      type: "short_text",
+      title: "whats_your_name",
+      ref: "whats_your_name",
+    };
+
+    const fieldValue = " ";
+
+    const qa = [["whats_your_name", " "]];
+
+    const ref = "whats_your_name";
+
+    const required = true;
+
+    const value = responseStore.nextAction(
+      form,
+      field,
+      fieldValue,
+      qa,
+      ref,
+      required
+    );
+
+    value.action.should.equal("error");
+  });
+
+  it("instructs the form to go to the next field if an answer evaluates to valid", () => {
+    const responseStore = new r.ResponseStore();
+
+    const field = {
+      type: "short_text",
+      title: "whats_your_name",
+      ref: "whats_your_name",
+    };
+
+    const fieldValue = "foo";
+
+    const qa = [["whats_your_name", "foo"]];
+
+    const ref = "whats_your_name";
+
+    let required = true;
+
+    const value = responseStore.nextAction(
+      form,
+      field,
+      fieldValue,
+      qa,
+      ref,
+      required
+    );
+
+    value.action.should.equal("navigate");
+
+    required = false;
+
+    responseStore.nextAction(form, field, fieldValue, qa, ref, required);
+
+    value.action.should.equal("navigate");
   });
 });
