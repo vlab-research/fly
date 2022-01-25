04c689210c919b22a11f5e738cb795be975c2c4c
Sophie <sophie@sssoja.com>
2022-01-25 13:21:50 +0100

    remove deprecated code

--- websurvey/src/App.svelte
+++ websurvey/src/App.svelte
@@ -2,7 +2,6 @@
 	import { Router, Route } from "svelte-routing";
 	import Home from "./routes/Home.svelte";
 	import Form from "./routes/Form.svelte";
-	// import Thankyou from "./routes/Thankyou.svelte";
 	import form from "../mocks/sample.json";
 
 	export let url = "";
@@ -16,8 +15,6 @@
 		<Route path="/:ref" let:params>
 			<Form ref={params.ref} {form} />
 		</Route>
-		<!-- <Route path=":ref" let:params>
-			<Thankyou ref={params.ref} {form} />
-		</Route> -->
+		
 	</Router>
 </main>
