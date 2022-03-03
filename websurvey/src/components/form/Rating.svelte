47fb3da404d3115b6939945855d7ce1c6df5f00a
Sophie <sophie@sssoja.com>
2022-03-03 16:19:58 +0100

    add basic styles to rating

--- websurvey/src/components/form/Rating.svelte
+++ websurvey/src/components/form/Rating.svelte
@@ -1,12 +1,14 @@
 <script>
     import { createEventDispatcher } from "svelte";
-    import { setRequired, stepCount } from "../../../lib/typewheels/form.js";
+    import { setRequired } from "../../../lib/typewheels/form.js";
 
     export let field, fieldValue, title;
 
+    const { properties } = field;
+
     const dispatch = createEventDispatcher();
 
-    const steps = field.properties.steps;
+    const { steps } = properties;
 
     const arr = [];
 
@@ -23,17 +25,19 @@
     for="field-{field.id}"
     class="text-2xl font-bold tracking-tight text-slate sm:text-xl whitespace-pre-line">{title}</label>
 <div class="space-y-2.5 mb-2">
-    <div class="flex flex-row items-center">
+    <div class="flex flex-row justify-between items-start mb-2">
         {#each arr as e, index}
-            <input
-                bind:group={fieldValue}
-                on:input={dispatch('add-field-value', fieldValue)}
-                required={field.validations.required ? setRequired : null}
-                type="radio"
-                name="steps"
-                value={e}
-                class="mr-2" />
-            <label for="label-{e}" class="sm:text-xl">{index}</label>
+            <div class="flex flex-col mr-4">
+                <input
+                    bind:group={fieldValue}
+                    on:input={dispatch('add-field-value', fieldValue)}
+                    required={field.validations.required ? setRequired : null}
+                    type="radio"
+                    name="steps"
+                    value={e}
+                    class="mr-2 mb-2" />
+                <label for="label-{e}" class="sm:text-xl mr-2">{index}</label>
+            </div>
         {/each}
     </div>
 </div>
