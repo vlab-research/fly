2c22642be6d896d3ff8e49cca516de4ad62d862a
Sophie <sophie@sssoja.com>
2022-03-10 18:57:35 +0100

    create reusable title component

---
+++ websurvey/src/components/text/Title.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let field;
+</script>
+
+<label
+    for="field-{field.id}"
+    class="text-sm sm:text-xl font-bold tracking-tight text-slate whitespace-pre-line">{field.title}</label>
