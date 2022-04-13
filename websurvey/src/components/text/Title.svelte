2d26516ba01bc32161d8c50487140a969cf783db
Sophie <sophie@sssoja.com>
2022-04-13 10:44:36 +0200

    conditionally use fieldset and legend

--- websurvey/src/components/text/Title.svelte
+++ websurvey/src/components/text/Title.svelte
@@ -2,7 +2,16 @@
     export let field;
 </script>
 
-<h2
-    class="text-sm md:text-lg font-bold tracking-tight text-slate whitespace-pre-line mb-4">
-    {field.title}
-</h2>
+{#if field.type === 'multiple_choice' || field.type === 'rating'}
+    <fieldset>
+        <legend
+            class="text-sm md:text-lg font-bold tracking-tight text-slate whitespace-pre-line mb-4">
+            {field.title}
+        </legend>
+    </fieldset>
+{:else}
+    <h2
+        class="text-sm md:text-lg font-bold tracking-tight text-slate whitespace-pre-line mb-4">
+        {field.title}
+    </h2>
+{/if}
