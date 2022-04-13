97aacb1bb84c26b2d3760b09dc52906a065f261b
Sophie <sophie@sssoja.com>
2022-04-13 11:05:07 +0200

    remove fieldset on multiple choice

--- websurvey/src/components/form/MultipleChoice.svelte
+++ websurvey/src/components/form/MultipleChoice.svelte
@@ -11,21 +11,19 @@
 
 <Title {field} />
 <div class="flex flex-col mb-4">
-    <fieldset>
-        {#each field.properties.choices as choice, index (choice.id)}
-            <label
-                for="choice-{choice.id}"
-                class=" flex flex-row items-center mb-2 border-solid rounded border-indigo-500 border-2 p-2 bg-indigo-100 transition-colors ease-linear hover:bg-indigo-200 text-sm md:text-lg text-slate-600 cursor-pointer">
-                <input
-                    bind:group={fieldValue}
-                    on:input={dispatch('add-field-value', fieldValue)}
-                    id="choice-{choice.id}"
-                    {required}
-                    type="radio"
-                    name="choices"
-                    value={choice.label}
-                    class="mr-2" />{choice.label}
-            </label>
-        {/each}
-    </fieldset>
+    {#each field.properties.choices as choice, index (choice.id)}
+        <label
+            for="choice-{choice.id}"
+            class="flex flex-row items-center md:w-3/4 mb-2 border-solid rounded border-indigo-500 border-2 p-2 bg-indigo-50 transition-colors ease-linear hover:bg-indigo-200 text-sm md:text-lg text-slate-600 cursor-pointer">
+            <input
+                bind:group={fieldValue}
+                on:input={dispatch('add-field-value', fieldValue)}
+                id="choice-{choice.id}"
+                {required}
+                type="radio"
+                name="choices"
+                value={choice.label}
+                class="mr-2" />{choice.label}
+        </label>
+    {/each}
 </div>
