0bb7bcdc2ff1fedf55a5fecd86fbac25dd42e11e
Sophie <sophie@sssoja.com>
2022-06-06 19:34:05 +0200

    add test coverage for good/bad responses

--- dashboard-server/queries/responses/response.test.js
+++ dashboard-server/queries/responses/response.test.js
@@ -127,20 +127,36 @@ describe('Response queries', () => {
         messages: '{"foo": "bar"}',
         shortcode: 231,
         userid: newUser.id,
-        title: 'Survey!',
+        title: 'Survey',
         metadata: '{}',
-        survey_name: 'Survey',
+        survey_name: 'Survey123',
+        translation_conf: '{}',
+      });
+
+      const survey2 = await Survey.create({
+        created: new Date(),
+        formid: '3hu23',
+        form: '{"form": "form detail"}',
+        messages: '{"foo": "bar"}',
+        shortcode: 123,
+        userid: newUser.id,
+        title: 'Other survey',
+        metadata: '{}',
+        survey_name: 'Survey321',
         translation_conf: '{}',
       });
 
       const MOCK_QUERY = `INSERT INTO responses(parent_surveyid, parent_shortcode, surveyid, shortcode, flowid, userid, question_ref, question_idx, question_text, response, seed, timestamp)
-    VALUES
-      ('${survey.id}', '101', '${
+      VALUES
+        ('${survey.id}', '101', '${
         survey.id
-      }', '101', 100001, '125', 'ref', 10, 'text', '{ "text": "last" }', '6789', current_date::timestamptz + interval '14 hour')
-     ,('${survey.id}', '101', '${
+      }', '101', 100001, '126', 'ref', 10, 'text', '{ "text": "last" }', '6789', current_date::timestamptz + interval '14 hour')
+       ,('${survey2.id}', '202', '${
+        survey2.id
+      }', '202', 100003, '124', 'ref', 10, 'text', '{ "text": "do not return me" }', '6789', (date '2019-04-18')::timestamptz + interval '12 hour')
+       ,('${survey.id}', '101', '${
         survey.id
-      }', '101', 100004, '125', 'ref', 10, 'text', '{ "text": "first" }', '6789', current_date::timestamptz + interval '10 hour')`;
+      }', '101', 100004, '126', 'ref', 10, 'text', '{ "text": "first" }', '6789', current_date::timestamptz + interval '10 hour')`;
 
       await vlabPool.query(MOCK_QUERY);
 
@@ -149,67 +165,85 @@ describe('Response queries', () => {
         survey: survey.survey_name,
       });
 
-      // responses.should.equal([
-      //   {
-      //     parent_surveyid: '1a1700ae-5fce-42e5-a0d9-4e02ea6da52d',
-      //     parent_shortcode: '101',
-      //     surveyid: '1a1700ae-5fce-42e5-a0d9-4e02ea6da52d',
-      //     flowid: '100004',
-      //     userid: '125',
-      //     question_ref: 'ref',
-      //     question_idx: '10',
-      //     question_text: 'text',
-      //     response: '{ "text": "first" }',
-      //     timestamp: '2022-06-06 10:00:00+00:00',
-      //     metadata: null,
-      //     pageid: null,
-      //     translated_response: null,
-      //   },
-      //   {
-      //     parent_surveyid: '1a1700ae-5fce-42e5-a0d9-4e02ea6da52d',
-      //     parent_shortcode: '101',
-      //     surveyid: '1a1700ae-5fce-42e5-a0d9-4e02ea6da52d',
-      //     flowid: '100001',
-      //     userid: '125',
-      //     question_ref: 'ref',
-      //     question_idx: '10',
-      //     question_text: 'text',
-      //     response: '{ "text": "last" }',
-      //     timestamp: '2022-06-06 14:00:00+00:00',
-      //     metadata: null,
-      //     pageid: null,
-      //     translated_response: null,
-      //   },
-      // ]);
+      responses.should.equal([
+        {
+          parent_surveyid: 'c741ab33-330a-4b75-b96f-ce390ac8c1d3',
+          parent_shortcode: '101',
+          surveyid: 'c741ab33-330a-4b75-b96f-ce390ac8c1d3',
+          flowid: '100004',
+          userid: '126',
+          question_ref: 'ref',
+          question_idx: '10',
+          question_text: 'text',
+          response: '{ "text": "first" }',
+          timestamp: '2022-06-06 10:00:00+00:00',
+          metadata: null,
+          pageid: null,
+          translated_response: null,
+        },
+        {
+          parent_surveyid: 'c741ab33-330a-4b75-b96f-ce390ac8c1d3',
+          parent_shortcode: '101',
+          surveyid: 'c741ab33-330a-4b75-b96f-ce390ac8c1d3',
+          flowid: '100001',
+          userid: '126',
+          question_ref: 'ref',
+          question_idx: '10',
+          question_text: 'text',
+          response: '{ "text": "last" }',
+          timestamp: '2022-06-06 14:00:00+00:00',
+          metadata: null,
+          pageid: null,
+          translated_response: null,
+        },
+      ]);
 
-      responses.length.should.equal(2);
-      responses[0].userid.should.equal('125');
+      responses[0].userid.should.equal('126');
       responses[0].response.should.equal('{ "text": "first" }');
-      responses[1].userid.should.equal('125');
+      responses[1].userid.should.equal('126');
       responses[1].response.should.equal('{ "text": "last" }');
 
-      it('should return no responses if the survey name is not found', async () => {
-        const responses2 = await Response.all({
-          email: user.name,
-          survey: 'Survey123',
-        });
-        responses2.length.should.equal(0);
-      });
-
       it('should return no responses if the user email is not found', async () => {
-        const responses3 = await Response.all({
+        const userNotFound = await Response.all({
           email: 'test4@vlab.com',
           survey: survey.survey_name,
         });
-        responses3.length.should.equal(0);
+        userNotFound.length.should.equal(0);
+      });
+
+      it('should return no responses if the survey name is not found', async () => {
+        const surveyNotFound = await Response.all({
+          email: user.email,
+          survey: 'Survey!',
+        });
+        surveyNotFound.length.should.equal(0);
+      });
+
+      it('should only return responses for the given survey', async () => {
+        const goodResponses = await Response.all({
+          email: user.email,
+          survey: survey.survey,
+        });
+        const badResponses = await Response.all({
+          email: user.email,
+          survey: survey2.survey,
+        });
+
+        badResponses.length.should.equal(1);
+        goodResponses.length.should.equal(2);
+        goodResponses.forEach(el => el.userid.should.equal(126));
+        goodResponses.forEach(el => el.userid.should.equal(126));
+        goodResponses.forEach(el =>
+          el.response.should.not.equal({ text: 'do not return me' }),
+        );
       });
     });
   });
 
-  describe('GET /', function() {
+  describe('GET /all', function() {
     it('responds with all responses in json', async () => {
       const response = await request(app)
-        .get('/?survey=Survey&after=2000&limit=100')
+        .get('/all?survey=Survey123&after=2000&limit=100')
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
         .expect(200);
