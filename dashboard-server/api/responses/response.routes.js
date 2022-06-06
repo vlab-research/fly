c96d5765b83d7d61e5ef0e1922972f5dba93f962
Sophie <sophie@sssoja.com>
2022-06-06 19:10:20 +0200

    reverse change to basepath

--- dashboard-server/api/responses/response.routes.js
+++ dashboard-server/api/responses/response.routes.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const controller = require('./response.controller');
 
 router
-  .get('/', controller.getAll)
-  .get('/first-and-last', controller.getFirstAndLast)
+  .get('/', controller.getFirstAndLast)
+  .get('/all', controller.getAll)
   .get('/form-data', controller.getFormDataCSV)
   .get('/csv', controller.getResponsesCSV);
 
