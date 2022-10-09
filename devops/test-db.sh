1758d521aaa3f83c9fcce802673b79b95afc6c0a
Nandan Rao <nandanmarkrao@gmail.com>
2022-10-09 21:03:07 +0200

    devops: test-db now has sql_safe_updates set

--- devops/test-db.sh
+++ devops/test-db.sh
@@ -7,3 +7,7 @@ docker stop vlab-cockroach && docker rm vlab-cockroach
 docker run --name vlab-cockroach -d -p $PORT:26257 cockroachdb/cockroach:$DB_VERSION start-single-node --insecure
 
 cat ./sql/* | docker run -i --net=host --rm cockroachdb/cockroach:$DB_VERSION sql --insecure --host localhost --port $PORT --database chatroach
+
+echo "set sql_safe_updates = false;" | docker run -i --net=host --rm cockroachdb/cockroach:$DB_VERSION sql --insecure --host localhost --port $PORT --database chatroach
+
+
