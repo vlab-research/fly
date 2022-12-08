af8acec8f001677e470928201dca89491b8fec45
Nandan Rao <nandanmarkrao@gmail.com>
2022-12-08 07:18:34 +0100

    fixed payment event to include all properties

--- replybot/lib/typewheels/transition.js
+++ replybot/lib/typewheels/transition.js
@@ -1,9 +1,9 @@
-const {exec, apply, act, update} = require('./machine')
-const {getForm} = require('./ourform')
-const {getUserInfo, sendMessage} = require('../messenger')
-const {responseVals} = require('../responses/responser')
-const {parseEvent, getPageFromEvent} = require('@vlab-research/utils')
-const {MachineIOError, iowrap} = require('../errors')
+const { exec, apply, act, update } = require('./machine')
+const { getForm } = require('./ourform')
+const { getUserInfo, sendMessage } = require('../messenger')
+const { responseVals } = require('../responses/responser')
+const { parseEvent, getPageFromEvent } = require('@vlab-research/utils')
+const { MachineIOError, iowrap } = require('../errors')
 const _ = require('lodash')
 const util = require('util')
 const Cacheman = require('cacheman')
@@ -14,14 +14,14 @@ function getPayment(userid, pageid, timestamp, event) {
   if (!payment) return
 
   const repeat = _.get(event, 'message.metadata.isRepeat')
-  if (repeat) return 
+  if (repeat) return
 
   // todo: handle errors from bad forms until
   // form validation exists
-  const {provider, details} = payment
+  const { provider } = payment
   if (!provider) return
 
-  return {userid, pageid, timestamp, provider, details}
+  return { userid, pageid, timestamp, ...payment }
 }
 
 class Machine {
@@ -48,7 +48,7 @@ class Machine {
     const page = getPageFromEvent(parsedEvent)
     const output = exec(state, parsedEvent)
     const newState = apply(state, output)
-    return {newState, output, page}
+    return { newState, output, page }
   }
 
   async actionsResponses(state, userId, timestamp, pageId, newState, output) {
@@ -58,16 +58,16 @@ class Machine {
     if (!newState.md) {
       throw new Error(`User without metadata: ${userId}. State: ${util.inspect(newState, null, 8)}`)
     }
-    const {startTime} = newState.md
+    const { startTime } = newState.md
 
     const pageToken = await iowrap('getPageToken', 'INTERNAL', this.getPageToken, pageId)
 
     const [form, surveyId] = await iowrap('getForm', 'INTERNAL', this.getForm,
-                                          pageId, shortcode, startTime)
+      pageId, shortcode, startTime)
 
     const user = await this.getUser(userId, pageToken)
 
-    const actions = act({form, user}, state, output)
+    const actions = act({ form, user }, state, output)
     const responses = responseVals(newState, upd, form, surveyId, pageId, userId, timestamp)
 
     return { actions, responses, pageToken, timestamp }
@@ -87,7 +87,7 @@ class Machine {
     const timestamp = event.timestamp
 
     if (!timestamp) {
-      return { publish: true, timestamp: Date.now(), user, error: { tag: 'CORRUPTED_MESSAGE',  event }}
+      return { publish: true, timestamp: Date.now(), user, error: { tag: 'CORRUPTED_MESSAGE', event } }
     }
 
     try {
@@ -101,52 +101,62 @@ class Machine {
       if (output.action === 'NONE') {
 
         // if not action, don't publish report, because the state doesn't change
-        return { publish: false,
-                 timestamp,
-                 user,
-                 page,
-                 newState,
-                 payment }
+        return {
+          publish: false,
+          timestamp,
+          user,
+          page,
+          newState,
+          payment
+        }
       }
 
     } catch (e) {
-      return { publish: false,
-               timestamp,
-               user,
-               page,
-               error: { tag: 'STATE_TRANSITION', message: e.message, stack: e.stack, state, event }}
+      return {
+        publish: false,
+        timestamp,
+        user,
+        page,
+        error: { tag: 'STATE_TRANSITION', message: e.message, stack: e.stack, state, event }
+      }
     }
     try {
 
       // Create successful report
-      const {actions, pageToken, responses} = await this.actionsResponses(state, user, timestamp, page, newState, output)
+      const { actions, pageToken, responses } = await this.actionsResponses(state, user, timestamp, page, newState, output)
       await this.act(actions, pageToken)
-      return { publish: true,
-               timestamp,
-               user,
-               page,
-               actions,
-               responses,
-               payment,
-               newState }
+      return {
+        publish: true,
+        timestamp,
+        user,
+        page,
+        actions,
+        responses,
+        payment,
+        newState
+      }
 
     } catch (e) {
       if (e instanceof MachineIOError) {
-        return { publish: true,
-                 timestamp,
-                 user,
-                 page,
-                 payment,
-                 newState,
-                 error: { ...e.details, tag: e.tag, message: e.message, stack: e.stack }}
+        return {
+          publish: true,
+          timestamp,
+          user,
+          page,
+          payment,
+          newState,
+          error: { ...e.details, tag: e.tag, message: e.message, stack: e.stack }
+        }
       } else {
-        return { publish: true,
-                 timestamp,
-                 user,
-                 page,
-                 payment,
-                 newState,
-                 error: { tag: 'STATE_ACTIONS', message: e.message, stack: e.stack }}
+        return {
+          publish: true,
+          timestamp,
+          user,
+          page,
+          payment,
+          newState,
+          error: { tag: 'STATE_ACTIONS', message: e.message, stack: e.stack }
+        }
       }
     }
   }
