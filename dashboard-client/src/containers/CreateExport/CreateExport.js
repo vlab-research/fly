55ab0b800ec9610115d00a6e2bdf3d367fd634f9
Nandan Rao <nandanmarkrao@gmail.com>
2024-09-30 22:18:25 -0400

    feat: dashboard: added wait back to export

--- dashboard-client/src/containers/CreateExport/CreateExport.js
+++ dashboard-client/src/containers/CreateExport/CreateExport.js
@@ -19,10 +19,14 @@ const CreateExport = () => {
   // Get available typeform forms or authorize with Typeform
   const onFinish = async (body) => {
     setLoading(true)
-
     // quick hack until we have select
-    body.metadata = body.metadata && body.metadata.split(',').map(x => x.trim());
+    body.metadata = body.metadata.trim();
+    body.metadata = body.metadata ? body.metadata.split(',').map(x => x.trim()) : null;
     await startExport(survey, body)
+
+    // artificial wait, hoping for exporter to catch up
+    await new Promise(resolve => setTimeout(resolve, 4000));
+
     setLoading(false)
     history.push('/exports')
   };
